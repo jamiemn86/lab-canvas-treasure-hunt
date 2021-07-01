9998f4b1bb1c7698d9d8e657121441c38ade2087
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-07-01 21:58:19 +0200

    Iteration 2 completed and manual tests passing when using console log

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ const height = canvas.height;
 function drawGrid() {
   // TODO: write the code of the function. The grid has 10 x 10 squares and
   // measures 500 x 500 pixels total
-  function verticalLines() {
+  function tenLines() {
+    // 10 vertical  lines
     for (let i = 0; i <= 10; i++) {
       context.beginPath();
       context.moveTo(i * 50, 0);
       context.lineTo(i * 50, 500);
       context.stroke();
     }
+    // 10 horizontal lines
     for (let i = 0; i <= 10; i++) {
       context.beginPath();
       context.moveTo(500, i * 50);
@@ -23,8 +25,35 @@ function drawGrid() {
       context.stroke();
     }
   }
-  verticalLines();
+  tenLines();
 }
+
+class Character {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  moveUp() {
+    this.row -= 1;
+  }
+  moveRight() {
+    this.col += 1;
+  }
+  moveDown() {
+    this.row += 1;
+  }
+  moveLeft() {
+    this.col -= 1;
+  }
+}
+
+const player = new Character(0, 0); // (0,0) = Initial position
+
+player.moveDown(); // Increase by 1 the value of player.row
+player.moveDown(); // Increase by 1 the value of player.row
+player.moveRight(); // Increase by 1 the value of player.col
+
+console.log(player.col, player.row); // => 1,2
 
 function drawEverything() {
   drawGrid();
