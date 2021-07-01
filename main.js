b972bf60dcdef8f48e997c61806f46ed50caf7bf
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-07-01 20:25:23 +0200

    Added HTML, CSS and JavaScript files prior to coding drawGrid

---
+++ main.js
@@ -0,0 +1,19 @@
+// main.js
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+const width = canvas.width;
+const height = canvas.height;
+
+// Iteration 1
+function drawGrid() {
+  // TODO: write the code of the function
+}
+
+function drawEverything() {
+  drawGrid();
+  // drawPlayer()
+  // drawTreasure()
+}
+
+drawEverything();
