a172dca281e799f65a88ef452133a7d6e36ef9ff
jamiemn86 <jamie.a.mollernielsen@gmail.com>
2021-07-02 19:02:45 +0200

    Attempts tto solve iteration 5, still failing manual tests

--- main.js
+++ main.js
@@ -105,6 +105,23 @@ function drawTreasure() {
   });
 }
 
+// Iteration 5
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+  if (e.keyCode == '38') {
+    console.log('up');
+  } else if (e.keyCode == '40') {
+    console.log('up');
+  } else if (e.keyCode == '37') {
+    console.log('left');
+  } else if (e.keyCode == '39') {
+    console.log('right');
+  }
+}
+
 function drawEverything() {
   drawGrid();
   drawPlayer();
